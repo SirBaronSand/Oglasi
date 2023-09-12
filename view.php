88f772c02190b00c6a600ed95250da257743fc76
SirBaronSand <30549311+SirBaronSand@users.noreply.github.com>
2023-09-12 20:32:03 +0200

    Add files via upload

--- view.php
+++ view.php
@@ -5,6 +5,67 @@
         public function prikaziFormu()
         {
             echo '
+            <head>
+  <title>Credit Card Validation</title>
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+      background-color: #f2f2f2;
+      padding: 20px;
+    }
+
+    h1 {
+      text-align: center;
+    }
+
+    .container {
+      max-width: 600px;
+      margin: 0 auto;
+      background-color: #ffffff;
+      padding: 20px;
+      border-radius: 8px;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    }
+
+    .form-group {
+      margin-bottom: 20px;
+    }
+
+    label {
+      display: block;
+      margin-bottom: 5px;
+      font-weight: bold;
+    }
+
+    input[type="text"] {
+      width: 95%;
+      padding: 10px;
+      border-radius: 4px;
+      border: 1px solid #ccc;
+    }
+
+    input[type="submit"] {
+      display: block;
+      width: 100%;
+      padding: 10px;
+      border-radius: 4px;
+      background-color: #4CAF50;
+      color: #ffffff;
+      font-size: 16px;
+      text-align: center;
+      border: none;
+      cursor: pointer;
+    }
+
+    input[type="submit"]:hover {
+      background-color: #45a049;
+    }
+
+    .error {
+      color: red;
+      font-size: 14px;
+    }
+  </style>
             
                 <form method="post" action="index.php">
                     Naslov: <br>
@@ -30,7 +91,7 @@
             {
                 echo "Naslov: <br><h1>{$oglas['naslov']}</h1>";
                 echo "Opis: <br><p>{$oglas['sadrzaj']}</p>";
-                echo "Cijena: <br><p>{$oglas['cijena']}</p>€";
+                echo "Cijena: <br><p>{$oglas['cijena']} €</p>";
                 echo "Kontakt: <br><p>{$oglas['kontakt']}</p>";
                 echo "Korisnik: <br><p>{$oglas['korisnicko_ime']}</p>";
             }
